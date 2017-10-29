6bc19dff9715f2b0cc0f2d389e6dd53010a86bf7
natalia <nataliav@gmail.com>
2017-10-29 00:43:50 -0400

    extracted temperature and speed settings to constants file

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import TeslaBattery from "./components/TeslaBattery/TeslaBattery";
 
-const counterDefaultVal = {
-  speed: {
-    title: "Speed",
-    unit: "mph",
-    step: 5,
-    min: 45,
-    max: 70
-  },
-  temperature: {
-    title: "Temperature",
-    unit: "º",
-    step: 10,
-    min: -10,
-    max: 40
-  }
-};
-
 class App extends Component {
   render() {
     return (
---
+++ src/constants/counterDefaultVal.js
@@ -0,0 +1,16 @@
+export const counterDefaultVal = {
+  speed: {
+    title: "Speed",
+    unit: "mph",
+    step: 5,
+    min: 45,
+    max: 70
+  },
+  temperature: {
+    title: "Temperature",
+    unit: "º",
+    step: 10,
+    min: -10,
+    max: 40
+  }
+};

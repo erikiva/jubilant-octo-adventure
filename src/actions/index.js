ada2b413ec2aaceacb71f24a8a84c4d41207d4ae
natalia <nataliav@gmail.com>
2017-10-29 00:40:29 -0400

    Added actions creators

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import { counterDefaultVal } from "../constants/counterDefaultVal";
+export const speedUp = value => {
+  return {
+    type: "SPEED_UP",
+    value,
+    step: counterDefaultVal.speed.step,
+    maxValue: counterDefaultVal.speed.max
+  };
+};
+
+export const speedDown = value => {
+  return {
+    type: "SPEED_DOWN",
+    value,
+    step: counterDefaultVal.speed.step,
+    minValue: counterDefaultVal.speed.min
+  };
+};

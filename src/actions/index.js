ebcf69fc433bdf4e5d84855172a0d80c93c757b8
natalia <nataliav@gmail.com>
2017-10-29 00:43:19 -0400

    Added rest of action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,3 +16,40 @@ export const speedDown = value => {
     minValue: counterDefaultVal.speed.min
   };
 };
+
+export const temperatureUp = value => {
+  return {
+    type: "TEMPERATURE_UP",
+    value,
+    step: counterDefaultVal.temperature.step,
+    maxValue: counterDefaultVal.temperature.max
+  };
+};
+
+export const temperatureDown = value => {
+  return {
+    type: "TEMPERATURE_DOWN",
+    value,
+    step: counterDefaultVal.temperature.step,
+    maxValue: counterDefaultVal.temperature.min
+  };
+};
+
+export const changeClimate = () => {
+  return {
+    type: "CHANGE_CLIMATE"
+  };
+};
+
+export const changeWheel = value => {
+  return {
+    type: "CHANGE_WHEEL",
+    value
+  };
+};
+
+export const updateStats = () => {
+  return {
+    type: "UPDATE_STATS"
+  };
+};

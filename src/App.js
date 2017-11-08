a9400496f003e29b91410d7ecf8f58f26ce265fb
natalia <nataliav@gmail.com>
2017-11-08 18:06:16 -0500

    Added Provider and connected the store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
-import "./App.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import TeslaCarContainer from "./containers/TeslaCarContainer";
+import TeslaStatsContainer from "./containers/TeslaStatsContainer";
+import TeslaSpeedCounterContainer from "./containers/TeslaSpeedCounterContainer";
+import TeslaTempCounterContainer from "./containers/TeslaTempCounterContainer";
+import TeslaClimateContainer from "./containers/TeslaClimateContainer";
+import TeslaWheelsContainer from "./containers/TeslaWheelsContainer";
+import TeslaNotice from "./components/TeslaNotice/TeslaNotice";
 import Header from "./components/Header/Header";
-import TeslaBattery from "./components/TeslaBattery/TeslaBattery";
+import "./App.css";
+import appReducer from "./reducers/teslaRangeApp";
+
+const store = createStore(
+  appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <TeslaBattery counterDefaultVal={counterDefaultVal} />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Header />
+          <div className="wrapper">
+            <form className="tesla-battery">
+              <h1 className="title-animation">Range Per Charge</h1>
+              <TeslaCarContainer />
+              <TeslaStatsContainer />
+              <div className="tesla-controls cf tesla-controls-animation">
+                <TeslaSpeedCounterContainer />
+                <div className="tesla-climate-container cf">
+                  <TeslaTempCounterContainer />
+                  <TeslaClimateContainer />
+                </div>
+                <TeslaWheelsContainer />
+              </div>
+              <TeslaNotice />
+            </form>
+          </div>
+        </div>
+      </Provider>
     );
   }
 }
